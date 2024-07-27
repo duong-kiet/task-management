36312898c72f87c5eae094026ac09184c2568c99
Duong Kiet <bintotet20041012@gmail.com>
2024-07-28 00:11:49 +0700

    Tinh nang dang ky

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 const controller = require('../controllers/task.controller')
 
-router.get("/tasks", controller.index)
+router.get("/", controller.index)
 
-router.get("/tasks/detail/:id", controller.detail)
+router.get("/detail/:id", controller.detail)
 
-router.patch("/tasks/change-status/:id", controller.changeStatus)
+router.patch("/change-status/:id", controller.changeStatus)
 
-router.patch("/tasks/change-multi", controller.changeMulti)
+router.patch("/change-multi", controller.changeMulti)
 
-router.post("/tasks/create", controller.create)
+router.post("/create", controller.create)
 
-router.patch("/tasks/edit/:id", controller.edit)
+router.patch("/edit/:id", controller.edit)
 
-router.delete("/tasks/delete/:id", controller.delete)
+router.delete("/delete/:id", controller.delete)
 
 module.exports = router
