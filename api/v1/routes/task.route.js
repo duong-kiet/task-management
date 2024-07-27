48deefc34197b573bd2cfe6a9ac8d5aeb6292055
Duong Kiet <bintotet20041012@gmail.com>
2024-07-27 19:38:32 +0700

    Doi trang thai nhieu cong viec

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -9,4 +9,6 @@ router.get("/tasks/detail/:id", controller.detail)
 
 router.patch("/tasks/change-status/:id", controller.changeStatus)
 
+router.patch("/tasks/change-multi", controller.changeMulti)
+
 module.exports = router
